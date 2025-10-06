99de2fd39a41e8ad42e9e3d822f47307391f8bcd
Ailten <faouzi26b@live.fr>
2025-10-06 20:11:28 +0200

    edit app

--- classFolder/Http.py
+++ classFolder/Http.py
@@ -5,12 +5,13 @@ class Http():
 
     def __init__(self, bot, port :str):
         self.app = Flask(__name__)
+        print(self.app)
         asyncio.run(self.app.run(host="127.0.0.1", port=port))
 
         self.bot = bot
         
 
-    @app.route('/event/sayInChannelDiscord', methods=['POST'])
+    @self.app.route('/event/sayInChannelDiscord', methods=['POST'])
     async def handleEvent():
         data = request.json # get package from event.
 
